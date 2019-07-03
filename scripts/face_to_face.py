f6c9b89da531cadc54e2a2801e506ad9a4863eac
Naho Yashiro <ny960907@gmail.com>
2019-07-03 06:13:18 +0000

    Scripts

--- scripts/face_to_face.py
+++ scripts/face_to_face.py
@@ -1,20 +1,51 @@
 #!/usr/bin/env python
 #encoding: utf8
-import rospy, cv2
+import rospy, cv2, math
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
+from geometry_msgs.msg import Twist
+from std_srvs.srv import Trigger
 
 class FaceToFace():
 	def __init__(self):
 		sub = rospy.Subscriber("/cv_camera/image_raw", Image, self.get_image)
 		self.bridge = CvBridge()
 		self.image_org = None
+		self.pub = rospy.Publisher("face", Image, queue_size=1)
+		self.cmd_vel = rospy.Publisher('cmd_vel', Twist, queue_size=1)
+		rospy.wait_for_service('/motor_on')
+		rospy.wait_for_service('/motor_off')
+		rospy.on_shutdown(rospy.ServiceProxy('/motor_off', Trigger).call)
+		rospy.ServiceProxy('/motor_on', Trigger).call()
+
+	def monitor(self,rect,org):
+		if rect is not None:
+			cv2.rectangle(org,tuple(rect[0:2]),tuple(rect[0:2]+rect[2:4]),(0,255,255),4)
+
+		self.pub.publish(self.bridge.cv2_to_imgmsg(org, "bgr8"))
 
 	def get_image(self,img):
 		try:
 			self.image_org = self.bridge.imgmsg_to_cv2(img, "bgr8")
 		except CvBridgeError as e:
 			rospy.logerr(e)
+
+	def rot_vel(self):
+		r = self.detect_face()
+		if r is None:
+			return 0.0
+
+		wid = self.image_org.shape[1]/2
+		pos_x_rate = (r[0] + r[2]/2 - wid)*1.0/wid 
+		rot = -0.25*pos_x_rate*math.pi
+		rospy.loginfo("detected %f",rot)
+		return rot
+
+	def control(self):
+		m = Twist()
+		m.linear.x = 0.0
+		m.angular.z = self.rot_vel()
+		self.cmd_vel.publish(m)
 
 	def detect_face(self):
 		if self.image_org is None:
@@ -28,12 +59,12 @@ class FaceToFace():
 		face = cascade.detectMultiScale(gimg,1.1,1,cv2.CASCADE_FIND_BIGGEST_OBJECT)
 		
 		if len(face) == 0:
+			self.monitor(None,org)
 			return None
 
 		r = face[0]
-		cv2.rectangle(org,tuple(r[0:2]),tuple(r[0:2]+r[2:4]),(0,255,255),4)
-		cv2.imwrite("/tmp/image.jpg",org)
-		return "detected"		
+		self.monitor(r,org)
+		return r		
 
 
 if __name__ == '__main__':
@@ -42,5 +73,5 @@ if __name__ == '__main__':
 
 	rate = rospy.Rate(10)
 	while not rospy.is_shutdown():
-		rospy.loginfo(fd.detect_face())
+		fd.control()
 		rate.sleep()
